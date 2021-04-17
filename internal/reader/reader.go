aeb6a96192ac5b7099dbcc512b100b974ad9691b
thiagoscherrer <46422437+thiago-scherrer@users.noreply.github.com>
2021-04-17 18:33:04 -0300

    add file reader (#6)

---
+++ internal/reader/reader.go
@@ -0,0 +1,25 @@
+package reader
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func Getlist() (string, error) {
+	list := os.Getenv("LIST")
+	if len(list) <= 0 {
+		log.Fatal("Need LIST env to check new blocklist")
+	}
+
+	dat, err := ioutil.ReadFile(list)
+	check(err)
+
+	return string(dat), nil
+}
