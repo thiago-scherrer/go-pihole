50bacdb88803164f0a7b97fcdb34ef43474b4a2e
thiago-scherrer <eu@thiago.info>
2021-04-17 18:30:34 -0300

    add file reader

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/thiago-scherrer/go-pihole/internal/reader"
+
+func main() {
+	reader.Getlist()
+}
---
+++ internal/reader/reader_test.go
@@ -0,0 +1,21 @@
+package reader
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGetlist(t *testing.T) {
+	os.Setenv("LIST", "mock.txt")
+
+	want := "example.com\n"
+	got, err := Getlist()
+
+	if err != nil {
+		t.Error("Got error on Getlist(): ", err)
+	}
+
+	if got != "example.com\n" {
+		t.Errorf("Erro on Getlist(). Want %v but got %v", want, got)
+	}
+}
