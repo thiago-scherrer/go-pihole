f498bc60f5c8e30d5995037c127765c6dca98e95
thiago-scherrer <eu@thiago.info>
2021-04-18 00:32:01 -0300

    first parse

--- internal/update/updater.go
+++ internal/update/updater.go
@@ -2,18 +2,39 @@ package update
 
 import (
 	"fmt"
-	"log"
+	"net/http"
 
 	"github.com/thiago-scherrer/go-pihole/internal/reader"
 )
 
-func Run() {
+func Run() error {
 	list, err := reader.Getlist()
 	if err != nil {
-		log.Fatal("Getlist() problem, got: ", err)
+		return err
 	}
 
 	for _, k := range list {
-		fmt.Println(k)
+		getDomains(k)
 	}
+	return nil
+}
+
+func getDomains(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	content := make([]byte, 1014)
+	c, err := resp.Body.Read(content)
+	if err != nil {
+		return nil, err
+	}
+
+	a := string(content[:c])
+
+	fmt.Println(a)
+
+	return nil, nil
 }
