16c5e549a5e0c24827a185fdc01123d38b73f5f2
thiago-scherrer <eu@thiago.info>
2021-04-18 17:41:03 -0300

    remove localhost

--- internal/update/cleaner.go
+++ internal/update/cleaner.go
@@ -1,9 +1,14 @@
 package update
 
 import (
-	"fmt"
+	"errors"
 	"log"
+	"os"
 	"regexp"
+)
+
+var (
+	errNeedOutput = errors.New("[error]: need OUTPUT env to save blocklist")
 )
 
 // Clean function can be used for remove noise from blocklist
@@ -23,9 +28,8 @@ func Clean(domains []byte) ([]string, error) {
 		log.Print(err)
 		return nil, err
 	}
-	fmt.Println(spc)
+	writeDom(spc)
 	return nil, nil
-
 }
 
 func rmComent(domains []byte) (string, error) {
@@ -52,7 +56,31 @@ func rmNonDomains(domains string) (string, error) {
 	myRegex, _ = regexp.Compile(`(127.0.0.1 )`)
 	altered = myRegex.ReplaceAllString(altered, "")
 
-	fmt.Println(altered)
-	return altered, nil
+	myRegex, _ = regexp.Compile(`(localhost )`)
+	altered = myRegex.ReplaceAllString(altered, "")
 
+	return altered, nil
+}
+
+func writeDom(domains string) ([]string, error) {
+	output := os.Getenv("OUTPUT")
+
+	if len(output) <= 1 {
+		log.Println(errNeedOutput)
+		return nil, errNeedOutput
+	}
+
+	f, err := os.OpenFile(output, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0755)
+	if err != nil {
+		log.Println("[error] failed on open file", err)
+		return nil, err
+	}
+
+	defer f.Close()
+	_, err = f.WriteString(domains)
+	if err != nil {
+		log.Println("[error] failed on write file", err)
+		return nil, err
+	}
+	return nil, nil
 }
