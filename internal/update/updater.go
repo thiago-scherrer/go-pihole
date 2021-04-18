809796a9cd66c72a61d63b45336cef405330a2dc
thiagoscherrer <46422437+thiago-scherrer@users.noreply.github.com>
2021-04-18 10:17:40 -0300

    add basic parse (#13)
    * first parse
    
    * log error
    
    * log error
    
    * add basic test

--- internal/update/updater.go
+++ internal/update/updater.go
@@ -3,17 +3,42 @@ package update
 import (
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/thiago-scherrer/go-pihole/internal/reader"
 )
 
-func Run() {
+func Run() error {
 	list, err := reader.Getlist()
 	if err != nil {
-		log.Fatal("Getlist() problem, got: ", err)
+		log.Println(err)
+		return err
 	}
 
 	for _, k := range list {
-		fmt.Println(k)
+		getDomains(k)
 	}
+	return nil
+}
+
+func getDomains(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	content := make([]byte, 1014)
+	c, err := resp.Body.Read(content)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	a := string(content[:c])
+
+	fmt.Println(a)
+
+	return nil, nil
 }
