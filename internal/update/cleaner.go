3a7ff226b7781dc78aae9be6d827f660e0c14527
thiago-scherrer <eu@thiago.info>
2021-04-20 00:00:26 -0300

    remove email from list

--- internal/update/cleaner.go
+++ internal/update/cleaner.go
@@ -44,7 +44,9 @@ func rmComent(domains []byte) (string, error) {
 }
 
 func rmNonDomains(domains string) (string, error) {
-	myRegex := regexp.MustCompile(`(?m)^repo(.*)|(0.0.0.0)|(127.0.0.1)|(localhost)|( )|(\n\n)|(\t)`)
+	myRegex :=
+		regexp.MustCompile(`(?m)^repo(.*)|(0.0.0.0)|(127.0.0.1)|(localhost)|(
+  )|(\n\n)|(\t)|(@)`)
 	altered := myRegex.ReplaceAllString(domains, "")
 
 	return altered, nil
