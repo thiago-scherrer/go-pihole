b2d51af96ff2d25a2728a2da1098abad81005d78
thiago-scherrer <eu@thiago.info>
2021-04-21 09:11:18 -0300

    add more mock test

--- internal/update/cleaner_test.go
+++ internal/update/cleaner_test.go
@@ -15,21 +15,48 @@ func TestClean(t *testing.T) {
 }
 
 func TestRmComent(t *testing.T) {
-	mock := []byte("exemple.com")
-	_, err := rmComent(mock)
 
-	if err != nil {
-		t.Error("Clean() error, got: ", err)
+	tests := []struct {
+		name string
+		mock string
+		want string
+	}{
+		{"Comment start", "#example.com", ""},
+		{"Comment middle", "example.com#This is commet", "example.com"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got, _ := rmComent([]byte(tt.mock)); got != tt.want {
+				t.Errorf("rmComent() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
 func TestRmNonDomains(t *testing.T) {
-	mock := "exemple.com"
-	_, err := rmNonDomains(mock)
-
-	if err != nil {
-		t.Error("Clean() error, got: ", err)
+	tests := []struct {
+		name string
+		mock string
+		want string
+	}{
+		{"Comment start", "repo example.com", ""},
+		{"Comment middle", "0.0.0.0 example.com", "example.com"},
+		{"Comment middle", "127.0.0.1 example.com", "example.com"},
+		{"Comment middle", "localhost example.com", "example.com"},
+		{"Comment middle", "\n\n example.com", "example.com"},
+		{"Comment middle", "\t example.com", "example.com"},
+		{"Comment middle", "@ example.com", "example.com"},
 	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got, _ := rmNonDomains(tt.mock); got != tt.want {
+				t.Errorf("rmComent() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+
 }
 
 func TestWriteDom(t *testing.T) {
